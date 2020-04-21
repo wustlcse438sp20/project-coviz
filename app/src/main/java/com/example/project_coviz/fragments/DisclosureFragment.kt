d6937c511d6a10b42586b26facdab5b0a5bbe920
Jason <jasondanielchoi@gmail.com>
2020-04-21 18:28:02 -0500

    Update disclosure with confirmation alert dialog.  Disable disclosure switch and datepicker if disclosed.

--- app/src/main/java/com/example/project_coviz/fragments/DisclosureFragment.kt
+++ app/src/main/java/com/example/project_coviz/fragments/DisclosureFragment.kt
@@ -1,5 +1,6 @@
 package com.example.project_coviz.fragments
 
+import android.content.DialogInterface
 import android.os.Bundle
 import android.text.method.LinkMovementMethod
 import android.util.Log
@@ -7,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import com.example.project_coviz.R
 import com.example.project_coviz.Settings
@@ -44,59 +46,81 @@ class DisclosureFragment : Fragment() {
                 Settings.disclose_year,
                 Settings.disclose_month,
                 Settings.disclose_day)
+            disclosureToggleSwitch.isEnabled = false
+            disclosureDatePicker.isEnabled = false
         }
 
         disclosureSubmitButton.setOnClickListener {
-            val disclose: Boolean = disclosureToggleSwitch.isChecked
-            val date = of(
-                disclosureDatePicker.year,
-                disclosureDatePicker.month+1,
-                disclosureDatePicker.dayOfMonth
-            )
-            val minDate = of(2019, 12, 1)
-            val today = now()
-            if (!disclose) {
-                Toast.makeText(it.context, "To disclose your own COVID-19 exposure you must toggle the switch, first.", Toast.LENGTH_LONG).show()
-                return@setOnClickListener
-            }
-            if(date.isAfter(today)) {
-                Toast.makeText(it.context, "Date is in the future.", Toast.LENGTH_LONG).show()
-                return@setOnClickListener
-            }
-            if(date.isBefore(minDate)) {
-                Toast.makeText(it.context, "Date appears to predate COVID-19.", Toast.LENGTH_LONG).show()
-                return@setOnClickListener
-            }
+            val alertDialog = AlertDialog.Builder(this.context!!)
+            alertDialog.setMessage(resources.getString(R.string.disclosure_warning))
+                .setCancelable(false)
+                .setPositiveButton("Yes", DialogInterface.OnClickListener {
+                        dialog, i ->
+                    disclose()
+                })
+                .setNegativeButton("No", DialogInterface.OnClickListener{
+                        dialog, id -> dialog.cancel()
+                })
+            val warning = alertDialog.create()
+            warning.setTitle("Are you sure?")
+            warning.show()
 
-            //Store values
-            Settings.disclosure = disclose
-            Settings.disclose_year = disclosureDatePicker.year
-            Settings.disclose_month = disclosureDatePicker.month
-            Settings.disclose_day = disclosureDatePicker.dayOfMonth
-
-            //send the data
-            val locationRepo = LocRepository(LocRoomDatabase.getDatabase(this.context!!).locDao())
-            val laterThan = Date.from(date.atStartOfDay().atZone( ZoneId.systemDefault()).toInstant())
-            locationRepo.getLatestLocations(laterThan) {
-                    locs ->
-                    val data = locs.map { LocationAndTimestamp(it.cell_token, it.time) }
-                    if(data.size > 0) {
-                        ApiClient.APIRepository.postLocationAndTimestamps(
-                            LocationAndTimestampData(
-                                data
-                            )
-                        )
-                        this.activity?.runOnUiThread(
-                            { Toast.makeText(this.activity, "We have notified our servers. Thank you for helping others avoid exposure.", Toast.LENGTH_LONG).show() }
-                        )
-                    }
-            }
-
-
-            //Turn off notifications
-            Settings.notify = false
         }
 
 
+    }
+
+    private fun disclose() {
+        val disclose: Boolean = disclosureToggleSwitch.isChecked
+        if (!disclose) {
+            Toast.makeText(this.context, "To disclose your own COVID-19 exposure you must toggle the switch, first.", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        val date = of(
+            disclosureDatePicker.year,
+            disclosureDatePicker.month+1,
+            disclosureDatePicker.dayOfMonth
+        )
+        val minDate = of(2019, 12, 1)
+        val today = now()
+
+        if(date.isAfter(today)) {
+            Toast.makeText(this.context, "Date is in the future.", Toast.LENGTH_LONG).show()
+            return
+        }
+        if(date.isBefore(minDate)) {
+            Toast.makeText(this.context, "Date appears to predate COVID-19.", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        //Store values
+        Settings.disclosure = disclose
+        Settings.disclose_year = disclosureDatePicker.year
+        Settings.disclose_month = disclosureDatePicker.month
+        Settings.disclose_day = disclosureDatePicker.dayOfMonth
+
+        //send the data
+        val locationRepo = LocRepository(LocRoomDatabase.getDatabase(this.context!!).locDao())
+        val laterThan = Date.from(date.atStartOfDay().atZone( ZoneId.systemDefault()).toInstant())
+        locationRepo.getLatestLocations(laterThan) {
+                locs ->
+            val data = locs.map { LocationAndTimestamp(it.cell_token, it.time) }
+            if(data.size > 0) {
+                ApiClient.APIRepository.postLocationAndTimestamps(
+                    LocationAndTimestampData(
+                        data
+                    )
+                )
+                this.activity?.runOnUiThread(
+                    { Toast.makeText(this.activity, resources.getString(R.string.disclosure_gratitude), Toast.LENGTH_LONG).show() }
+                )
+            }
+        }
+
+        disclosureToggleSwitch.isEnabled = false
+        disclosureDatePicker.isEnabled = false
+        //Turn off notifications
+        Settings.notify = false
     }
 }
