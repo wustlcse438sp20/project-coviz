b64bcdb16a464824bc5bfddac68ee9c9840d0324
MudterC <clay.mudter@wustl.edu>
2020-04-23 14:57:56 -0500

    working on watchlist

--- app/src/main/java/com/example/project_coviz/fragments/PredictionFragment.kt
+++ app/src/main/java/com/example/project_coviz/fragments/PredictionFragment.kt
@@ -8,7 +8,10 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.project_coviz.*
+import com.example.project_coviz.MapsActivity
+import com.example.project_coviz.R
+import com.example.project_coviz.Settings
+import com.example.project_coviz.WatchlistAdapter
 import com.example.project_coviz.api.ApiClient
 import com.example.project_coviz.api.LocationAndTimestampData
 import com.example.project_coviz.db.LocRepository
@@ -41,30 +44,49 @@ class PredictionFragment : Fragment() {
 
         var watchlist: HashMap<S2LatLng, Int> = HashMap()
 
+
+//        var casesLiveData: MutableLiveData<LocationAndTimestampData> = MutableLiveData()
+//
+//        casesLiveData.observe(activity as MapsActivity, Observer { cases ->
+//
+//        })
         val calendar: Calendar = Calendar.getInstance()
         calendar.add(Calendar.HOUR, -1 * Settings.HOURS_OF_DATA)
         val date: Date = calendar.time
-        locationRepo = LocRepository(LocRoomDatabase.getDatabase(activity as MapsActivity).locDao())
-        locationRepo.getLatestLocations(date) { locations ->
-            for (userLocation in locations) {
-                var casesLiveData: MutableLiveData<LocationAndTimestampData> = MutableLiveData()
-                casesLiveData.observe(activity as MapsActivity, Observer {
-                    val cellLatLng = S2CellId.fromToken(userLocation.cell_token).toLatLng()
-                    watchlist.put(cellLatLng, it.data.size)
-                    adapter.notifyDataSetChanged()
-                })
-                ApiClient.APIRepository.getLocationAndTimestamps(casesLiveData, userLocation.cell_token)
-                println(userLocation.id)
-            }
-        }
-
-        adapter = WatchlistAdapter(watchlist.toList())
-
-        adapter.notifyDataSetChanged()
-
-        highest_rise.adapter = adapter
-
-        highest_rise.layoutManager = LinearLayoutManager(this.context)
+//        locationRepo = LocRepository(LocRoomDatabase.getDatabase(activity as MapsActivity).locDao())
+//        ApiClient.APIRepository.locationAndTimestamps.observe(this, androidx.lifecycle.Observer {
+//            var infectionLocations = it;
+//            locationRepo.getLatestLocations(date) { locations ->
+//                for (infectionLocation in infectionLocations.data) {
+//                    for (userLocation in locations) {
+//                        watchlist.clear()
+//                        var token = S2CellId.fromToken(userLocation.cell_token).toLatLng()
+//                        if (S2CellId.fromToken(infectionLocation.cell_token).intersects(
+//                                S2CellId.fromToken(
+//                                    userLocation.cell_token
+//                                )
+//                            )
+//                        ) {
+//                            if (watchlist.containsKey(token)) {
+//                                watchlist.put(token, watchlist.get(token)!! + 1)
+//                            } else {
+//                                watchlist.put(
+//                                    S2CellId.fromToken(userLocation.cell_token).toLatLng(),
+//                                    1
+//                                )
+//                            }
+//
+//                        }
+//                    }
+//                }
+//                adapter.notifyDataSetChanged()
+//            }
+//        })
+//        adapter = WatchlistAdapter(watchlist.toList())
+//
+//        highest_rise.adapter = adapter
+//
+//        highest_rise.layoutManager = LinearLayoutManager(this.context)
     }
 
 }
